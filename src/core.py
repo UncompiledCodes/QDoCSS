cd2e0b5d22a0fca01ca53d596c78dbe984dffed8
MohammadHossein <themhsalimi@gmail.com>
2021-06-08 22:37:26 +0430

    build: add hamiltonian calculation def

--- src/core.py
+++ src/core.py
@@ -1,38 +1,36 @@
 from qutip import *
 from numpy import pi
 
-
+# Identity matrix
 si = qeye(2)
+# pauli x
 sx = sigmax()
+# pauli y
 sy = sigmay()
+# pauli z
 sz = sigmaz()
-sm = sigmam()
-sp = sigmap()
-n = 3  # number of nuclei
-
-m = 1  # number of electrons
-
-extmagfield_m = 1  # external magnetic field of m
-
-extmagfield_n = Qobj(
-    [[0, 0, 0], [0, 0, 0], [0.3, 0.3, 0.3]]
-)  # external magnetic field of n
-
-j = Qobj([[0, 0, 0], [0, 0, 0], [0, 0, 0]])  # value of exchange interaction constant: J
-
-a = Qobj(
-    [[0, 0, 0], [0, 0, 0], [0.99925, 0.625147, 0.551273]]
-)  # value of exchange interaction constant: A
-
+# number of nuclei
+n = 3
+# number of electrons
+m = 1
+# external magnetic field of m
+extmagfield_m = 1
+# external magnetic field of n
+extmagfield_n = Qobj([[0, 0, 0], [0, 0, 0], [0.3, 0.3, 0.3]])
+# value of exchange interaction constant: J
+JJ = Qobj([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+# value of exchange interaction constant: A
+AA = Qobj([[0, 0, 0], [0, 0, 0], [0.99925, 0.625147, 0.551273]])
+# value of exchange interaction constant: Gama
 gama = Qobj(
     [
         [0.0124425, 0.0806628, 0.00999575],
         [0.0550028, 0.0758354, 0.07346340],
         [0.0972069, 0.0723954, 0.07405450],
     ]
-)  # value of exchange interaction constant: Gama
-
-t = (10 * 2 * pi) / extmagfield_m  # value of time
+)
+# value of time
+t = (10 * 2 * pi) / extmagfield_m
 
 # spin oprators
 def spin_operators(N):
@@ -47,39 +45,73 @@ def spin_operators(N):
     Sx = []
     Sy = []
     Sz = []
-    Sp = []
-    Sm = []
 
     for n in range(N):
         x_list = []
         y_list = []
         z_list = []
-        p_list = []
-        m_list = []
+
         for m in range(N):
             x_list.append(si)
             y_list.append(si)
             z_list.append(si)
-            p_list.append(si)
-            m_list.append(si)
 
         x_list[n] = sx
         y_list[n] = sy
         z_list[n] = sz
-        p_list[n] = sp
-        m_list[n] = sm
 
         Sx.append(tensor(x_list))
         Sy.append(tensor(y_list))
         Sz.append(tensor(z_list))
-        Sp.append(tensor(p_list))
-        Sm.append(tensor(m_list))
-    # print(Qobj(Sx[1]))
-    return [Sx, Sy, Sz, Sp, Sm]
-    # return Qobj(Sx)
+
+    return [Sx, Sy, Sz]
 
 
-Sx, Sy, Sz, Sp, Sm = spin_operators(2)
+def hamiltonian(
+    m, n, Sx, Sy, Sz, Ix, Iy, Iz, JJ, AA, gama, extmagfield_m, extmagfield_n
+):
+    """calculating system hamiltonian
 
-for i in range(2):
-    print(Sx[i])
+    Args:
+        m (int): number of electrons
+        n (int): number of nuclei
+        Sx (matrix): coupled spin
+        Sy (matrix): coupled spins
+        Sz (matrix): coupled spins
+        Ix (matrix): environmental spin
+        Iy (matrix): environmental spin
+        Iz (matrix): environmental spin
+        JJ (matrix): exchange interaction constant
+        AA (matrix): exchange interaction constant
+        gama (matrix): exchange interaction constant
+        extmagfield_m (int): external magnetic field of m
+        extmagfield_n (matrix): external magnetic field of n
+
+    Returns:
+        matrix: hamiltonian of he entire system
+    """
+    # HS & HB are bare hamiltonians of the central system and bath
+    # V is the system-bath interaction
+    HS = HB = V = 0
+    for i in range(m):
+        HS += extmagfield_m * Sx[i] + extmagfield_m * Sy[i] + extmagfield_m * Sz[i]
+        for j in range(m):
+            if m != 1:
+                if i == j:
+                    continue
+            HS += JJ * Sx[i] * Sx[j] + JJ * Sy[i] * Sy[j] + JJ * Sz[i] * Sz[j]
+
+    for i in range(n):
+        HB += extmagfield_n * Ix[i] + extmagfield_n * Iy[i] + extmagfield_n * Iz[i]
+        for j in range(n):
+            if n != 1:
+                if i == j:
+                    continue
+            HB += gama * Ix[i] * Ix[j] + gama * Iy[i] * Iy[j] + gama * Iz[i] * Iz[j]
+
+    for i in range(m):
+        for j in range(n):
+            V += AA * Sx[i] * Ix[j] + AA * Sy[i] * Iy[j] + AA * Sz[i] * Iz[j]
+
+    HH = HS + HB + V
+    return HH
